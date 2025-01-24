0abd0784a889ec9cfeaaa093cc7e00a734be2140
Fullstomic-Developer <develop.masaharu@gmail.com>
2025-01-24 02:52:38 +0000

    [add]検索データを追加しました。

---
+++ public/search_system/search-data.js
@@ -0,0 +1,62 @@
+export class SearchData {
+  monster_list = [
+    {
+      name: "てけてけ",
+      item: "スコップ",
+      action: "掘る",
+      result: "鬼ごっこ",
+    },
+    {
+      name: "水子",
+      item: "スコップ",
+      action: "掘る",
+      result: "アイテム",
+    },
+    {
+      name: "気が荒い付喪神",
+      item: "スコップ",
+      action: "壁殴る",
+      result: "襲われる",
+    },
+    {
+      name: "いじめられっ子",
+      item: "スコップ",
+      action: "壁殴る",
+      result: "隠し通路",
+    },
+    {
+      name: "金次郎",
+      item: "本",
+      action: "投げる",
+      result: "アイテム",
+    },
+    {
+      name: "病気のお母さん",
+      item: "本",
+      action: "投げる",
+      result: "写真アイテム",
+    },
+    {
+      name: "グリモア",
+      item: "本",
+      action: "片づける",
+      result: "捕まれる",
+    },
+    {
+      name: "いたずらっ子",
+      item: "本",
+      action: "片づける",
+      result: "隠し通路",
+    },
+  ];
+
+  hint_image_list = [
+    ["右手形", "2本", "包帯無し", "父頭無し", "枯れた花"],
+    ["左手形", "二本曲線", "消毒なし", "母頭無し", "割れた花瓶"],
+  ];
+  hint_sound_text_list = [
+    ["猫", "1回", "がっしゃーん"],
+    ["遠吠え", "2回", "パリーン"],
+  ];
+  on_get_monster_list() {}
+}
--- public/search_system/search-process.js
+++ public/search_system/search-process.js
@@ -1,9 +1,20 @@
+import { SearchData } from "./search-data.js";
 export class SearchProcess {
   OnGetData(search_data) {
     let search_data_array = search_data.split(",");
     return this.OnSearchProcess(search_data_array);
   }
   OnSearchProcess(data) {
+    let search_data_instance = new SearchData();
+    let monster_list = search_data_instance.monster_list;
+    let hint_image_list = search_data_instance.hint_image_list;
+    let hint_sound_text_list = search_data_instance.hint_sound_text_list;
+
+    let hint_subscript_list = {
+      hint_1: data[1] / 4,
+      hint_2: (data[1] / 2) % 2,
+      hint_3: data[1] / 2,
+    };
     return this.OnDisplayResultData(data);
   }
   OnDisplayResultData(result_data) {
